ac550ea08b416f95fe9d967df9ee9467e1b6b0fe
s1uad_dw <s1uad.dw@gmail.com>
2023-10-11 08:13:32 +0300

    generators is ready'

--- Lists/src/User.java
+++ Lists/src/User.java
@@ -1,11 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class User {
     public String name;
     public int age;
     public Gender gender;
+    User(){
+        name = "";
+        age = 0;
+    }
     User(String n, int a, Gender g){
         name = n;
         age = a;
         gender = g;
+    }
+    public List<User> sortByGender(List<User> list, Gender gender){
+        return list.stream().filter(user -> user.gender == gender).collect(Collectors.toList());
     }
     @Override
     public String toString() {
